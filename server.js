1d71eafbf24f326b9bfa3ca7f99699654084e59b
AlaEddine01 <61976331+AlaEddine01@users.noreply.github.com>
2021-06-10 20:51:07 +0100

    deployment prepation

--- server.js
+++ server.js
@@ -11,13 +11,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 //db config
 connectDB();
 
 //api routes
-app.use("/",require('./routes'))
+app.use("/", require("./routes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("front_products/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //server listening
 app.listen(port, () => console.log(`Listening on localhost: ${port}`));
